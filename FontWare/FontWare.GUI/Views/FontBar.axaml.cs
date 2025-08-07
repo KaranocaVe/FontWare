6107f7332b81bafebd392658f34b8dee531d1a94
KaranocaVe <karanocave@qq.com>
2025-08-07 13:51:10 +0800

    Add SukiUI as a submodule

---
+++ FontWare/FontWare.GUI/Views/FontBar.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace FontWare.GUI.Views
+{
+    public partial class FontBar : UserControl
+    {
+        public FontBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
